ad0750c8c739f574d37a66ee58008f791a328263
Adelino Muecália Eduardo <amed.develop@outlook.com>
2024-09-14 20:51:07 +0100

    Implementação da extração dos dados dos Números: 1. Números a espera de imagens (GetNumerosEsperaImagens) 2. Estatística de Números a espera de imagens (GetEstatisticaNumerosEsperaImagens) 3. Números removidos (GetNumerosRemovidos) 4. Números Terminados (GetNumerosTerminados)

---
+++ TotalAgilityApi/Controllers/NumeroController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TotalAgilityApi.Infraestrutura.Interfaces;
+using TotalAgilityApi.Wrappers;
+
+namespace TotalAgilityApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumeroController : ControllerBase
+    {        
+        private readonly INumeroRepository _iNumeroRepository;
+        
+        public NumeroController(INumeroRepository iNumeroRepository)
+        {            
+            _iNumeroRepository = iNumeroRepository;
+        }
+
+        [HttpGet("numerosEsperaImagens")]
+        public async Task<ActionResult<Response<string>>> GetNumerosEsperaImagens(CancellationToken cancellationToken)
+        {
+            var response = await _iNumeroRepository.GetNumerosEsperaImagens(cancellationToken);
+            if (response.Succeeded)
+                return Ok(response.Message);
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("estatisticaNumerosEsperaImagens")]
+        public async Task<ActionResult<Response<string>>> GetEstatisticaNumerosEsperaImagens(int NumeroDias, CancellationToken cancellationToken)
+        {
+            var response = await _iNumeroRepository.GetEstatisticaNumerosEsperaImagens(NumeroDias, cancellationToken);
+            if (response.Succeeded)
+                return Ok(response.Message);
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("numerosRemovidos")]
+        public async Task<ActionResult<Response<string>>> GetNumerosRemovidos(CancellationToken cancellationToken)
+        {
+            var response = await _iNumeroRepository.GetNumerosRemovidos(cancellationToken);
+            if (response.Succeeded)
+                return Ok(response.Message);
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("numerosTerminados")]
+        public async Task<ActionResult<Response<string>>> GetNumerosTerminados(CancellationToken cancellationToken)
+        {
+            var response = await _iNumeroRepository.GetNumerosTerminados(cancellationToken);
+            if (response.Succeeded)
+                return Ok(response.Message);
+            return BadRequest(response.Message);
+        }
+
+    }
+}
